e3aacd14c5da2679df18915fce14694efdfcc07f
Jasmina Blazevic <jasmina.blaze.89@gmail.com>
2021-12-16 15:34:44 +0100

    changing pokemonsList in script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let pokemonsList=[{name:"ivysaur",height:"1", type:["grass","poison"]},{name:"wartortle", height:"1", type:"water"}, {name:"cubchoo", height:"0.5", type:"ice"}];
+let pokemonsList=[
+    {name:"Venusaur",height:2 , type:["grass","poison"]},
+    {name:"Wartortle", height:"1", type:"water"}, 
+    {name:"Cubchoo", height: 0.5, type:"ice"}];
+    
+ // adding the loop to create list of pokemons and highlighting the largest one
+for (let i = 0; i < pokemonsList.length; i++) {
+     document.write('<br>', '<br>' + pokemonsList[i].name + (',  height:   ') + pokemonsList[i].height) ;
+    if (pokemonsList[i].height >= 2) {
+      document.write( ' - Wow, that\'s big!' ) ;
+    }
+  } 
+ 
