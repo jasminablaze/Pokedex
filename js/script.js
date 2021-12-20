194df0e682c720957724baec6992e32e87f3d63a
Jasmina Blazevic <jasmina.blaze.89@gmail.com>
2021-12-20 22:48:19 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let pokemonsList=[
     
  // adding the loop to create list of pokemons and highlighting the largest one
 for (let i = 0; i < pokemonsList.length; i++) {
-     document.write('<br>', '<br>' + pokemonsList[i].name + (',  height:   ') + pokemonsList[i].height) ;
+     document.write('<br>', '<br>' + pokemonsList[i].name + ', (height:   ' + pokemonsList[i].height +')') ;
     if (pokemonsList[i].height >= 2) {
       document.write( ' - Wow, that\'s big!' ) ;
     }
