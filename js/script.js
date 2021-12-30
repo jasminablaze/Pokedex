e59d6df9d778d3bb01a940b39e4eaf7a6c938733
Jasmina Blazevic <jasmina.blaze.89@gmail.com>
2021-12-30 17:50:35 +0100

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,51 @@
-let pokemonsList=[
-    {name:"Venusaur",height:2 , type:["grass","poison"]},
-    {name:"Wartortle", height:"1", type:"water"}, 
-    {name:"Cubchoo", height: 0.5, type:"ice"}];
+
+//Created an IIFE to wrap the previous 'global variables' and turn them into 'local variables'.*/
+let pokemonRepository = (function() {
     
- // adding the loop to create list of pokemons and highlighting the largest one
-for (let i = 0; i < pokemonsList.length; i++) {
-     document.write('<br>', '<br>' + pokemonsList[i].name + ', (height:   ' + pokemonsList[i].height +')') ;
-    if (pokemonsList[i].height >= 2) {
-      document.write( ' - Wow, that\'s big!' ) ;
-    }
-  } 
- 
+  let pokemonList = [
+      {name:"Venusaur",height:2 , type:["grass","poison"]},
+      {name:"Wartortle", height:"1", type:"water"}, 
+      {name:"Cubchoo", height: 0.5, type:"ice"}
+  ];
+  function nameList (name){
+      return (name === pokemonList.name);
+  }
+  pokemonList.filter(nameList);
+  
+  //Function to return all items within the pokemonList array.
+    function getAll() {
+      return pokemonList;
+  }
+
+  //Created function to, when required, add new pokemon to the pokemonList array.
+  function add(newPokemon) {
+      pokemonList.push(newPokemon);
+      //Explanation: if-else statement to check whether new pokemon entered is an object.
+      if (typeof newPokemon === 'object') {
+          pokemonList.push(newPokemon);
+      } else {
+          console.log('Did not add - this must be an object');
+      }
+  }
+     return {
+      getAll: getAll,
+      add: add
+  };
+})();
+
+//Added an if-else statement to highlight the largest pokemon in the array.*/
+function printArrayDetails(list) {
+  list.forEach(function(pokemon) {
+      if (pokemon.height >= 2.0) {
+         document.write('<br>', '<br>'  + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big! ');
+      } else {
+         document.write('<br>', '<br>' + pokemon.name + ', (height:   ' + pokemon.height +')') ;
+          
+
+      }
+  });
+}
+
+printArrayDetails(pokemonRepository.getAll());
+    
+
